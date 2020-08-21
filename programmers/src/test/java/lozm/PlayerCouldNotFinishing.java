d66a3478af37e54ce03c8a7da59613f27a1fe0a1
junlee <laonzenamoon@gmail.com>
2020-08-21 10:14:40 +0900

    - developing 'PlayerCouldNotFinishing' code.

--- programmers/src/test/java/lozm/PlayerCouldNotFinishing.java
+++ programmers/src/test/java/lozm/PlayerCouldNotFinishing.java
@@ -1,12 +1,10 @@
 package lozm;
 
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,39 +12,43 @@ public class PlayerCouldNotFinishing {
 
     @Test
     void test() {
-        //Given
-        //Set the participants
-        List<String> participantArray = new ArrayList<>();
+        try {
+            //Given
+            //Set the participants
+            List<String> participantArray = new ArrayList<>();
 
-        int participantSize = ThreadLocalRandom.current().nextInt(1, 100000);
-        for (int i = 0; i < participantSize; i++) {
-            participantArray.add(getRandomName());
+            int participantSize = ThreadLocalRandom.current().nextInt(1, 100000);
+            for (int i = 0; i < participantSize; i++) {
+                participantArray.add(getRandomName());
+            }
+            String[] participant = participantArray.toArray(new String[participantArray.size()]);
+
+            //Set the completions
+            int randomInt = ThreadLocalRandom.current().nextInt(1, participantSize);
+            participantArray.remove(randomInt);
+            String[] completion = participantArray.toArray(new String[participantArray.size()]);
+
+            //When
+            String uncompletion = solution(participant, completion);
+
+            //Then
+            assertThat(participant.length-1).isEqualTo(completion.length);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        String[] participant = (String[]) participantArray.toArray();
-
-        //Set the completions
-        int randomInt = ThreadLocalRandom.current().nextInt(1, participantSize);
-        participantArray.remove(randomInt);
-        String[] completion = (String[]) participantArray.toArray();
-
-        //When
-        String uncompletion = solution(participant, completion);
-
-        //Then
-        assertThat(participant.length-1).isEqualTo(completion.length);
     }
 
     public String getRandomName() {
         char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
         int nameLength = ThreadLocalRandom.current().nextInt(1, 20);
-        StringBuilder randomName = new StringBuilder();
+        StringBuffer randomName = new StringBuffer();
 
         for (int i = 0; i < nameLength; i++) {
             int randomAlphabet = ThreadLocalRandom.current().nextInt(0, alphabet.length);
             randomName.append(alphabet[randomAlphabet]);
         }
 
-        return getRandomName();
+        return randomName.toString();
     }
 
     public String solution(String[] participant, String[] completion) {
