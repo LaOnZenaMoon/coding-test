b2de33d51de7d92f0f243cf9d3f78a27f2570a7e
junlee <laonzenamoon@gmail.com>
2020-09-02 18:42:32 +0900

    - develop BreadthFirstSearch - develop DepthFirstSearch

--- default/src/main/java/lozm/algorithm/DepthFirstSearch.java
+++ default/src/main/java/lozm/algorithm/DepthFirstSearch.java
@@ -1,6 +1,24 @@
 package lozm.algorithm;
 
+import java.util.LinkedList;
+
 public class DepthFirstSearch {
+
+    private int nodeNumber;
+    private LinkedList<Integer> adjacencyNode[];
+
+    public DepthFirstSearch(int nodeNumber) {
+        this.nodeNumber = nodeNumber;
+        adjacencyNode = new LinkedList[nodeNumber];
+
+        for (int i = 0; i < nodeNumber; i++) {
+            adjacencyNode[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int sourceNodeIndex, int targetNodeIndex) {
+        adjacencyNode[sourceNodeIndex].add(targetNodeIndex);
+    }
 
     public void perform(Object visitedNode) {
 
