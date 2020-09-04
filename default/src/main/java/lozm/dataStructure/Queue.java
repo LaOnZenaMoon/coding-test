62679f691f332cde2bbcfced03a27759926d93ff
junlee <laonzenamoon@gmail.com>
2020-09-04 10:33:50 +0900

    - develop Queue

---
+++ default/src/main/java/lozm/dataStructure/Queue.java
@@ -0,0 +1,37 @@
+package lozm.dataStructure;
+
+public class Queue {
+
+    private SinglyLinkedList instance;
+
+    public Queue() {
+        this.instance = new SinglyLinkedList();
+    }
+
+    public void enqueue(Object input) {
+        instance.insertNodeAt(input, instance.getSize());
+    }
+
+    public SinglyLinkedList.Node dequeue() {
+        return instance.removeNodeAt(0);
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue("test1");
+        queue.enqueue("test2");
+        queue.enqueue("test3");
+        queue.enqueue("test4");
+
+        SinglyLinkedList.Node dequeue1 = queue.dequeue();
+        System.out.println("dequeue1 = " + dequeue1);
+        SinglyLinkedList.Node dequeue2 = queue.dequeue();
+        System.out.println("dequeue2 = " + dequeue2);
+        SinglyLinkedList.Node dequeue3 = queue.dequeue();
+        System.out.println("dequeue3 = " + dequeue3);
+        SinglyLinkedList.Node dequeue4 = queue.dequeue();
+        System.out.println("dequeue4 = " + dequeue4);
+
+    }
+
+}
