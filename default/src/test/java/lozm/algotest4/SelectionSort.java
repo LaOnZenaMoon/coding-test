56b594ad6dad065a34606c2908a06c58fefb0520
junlee <laonzenamoon@gmail.com>
2020-09-07 11:00:48 +0900

    - test algorithm4

---
+++ default/src/test/java/lozm/algotest4/SelectionSort.java
@@ -0,0 +1,37 @@
+package lozm.algotest4;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] data = {3,5,2,1,4,6,5,9};
+
+        int[] perform = perform(data);
+
+        for (int item : perform) {
+            System.out.println("item = " + item);
+        }
+    }
+
+    private static int[] perform(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int minIndex = 0;
+            int minValue = Integer.MAX_VALUE;
+
+            for (int j = i; j < data.length; j++) {
+                if(minValue > data[j]) {
+                    minIndex = j;
+                    minValue = data[j];
+                }
+            }
+
+            //Swap
+            int temp = data[i];
+            data[i] = data[minIndex];
+            data[minIndex] = temp;
+        }
+
+        return data;
+    }
+
+
+}
